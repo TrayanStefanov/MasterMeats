d1fe3eb47b6717886c9eabdbd4d991ef1c0c5065
TrayanStefanov <trayanstefanov95@gmail.com>
2025-09-03 21:26:13 +0300

    User Auth controller imports fixed.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import { redis } from "../lib/redis";
-import User from "../models/user.model";
+import { redis } from "../lib/redis.js";
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 const generateTokens = (userId) => {
