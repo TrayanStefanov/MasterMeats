b01cde236003e5f0b0bf67c55e3f341444562ff8
TrayanStefanov <trayanstefanov95@gmail.com>
2025-09-10 17:31:05 +0300

    App v0.1. Starting structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
+import React from 'react'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HeroSection from './sections/HeroSection'
+import ReviewsSection from './sections/ReviewsSection'
+import ProductsSection from './sections/ProductsSection'
+import AboutSection from './sections/AboutSection'
+import ContactsSection from './sections/ContactsSection'
+import CoreValuesSection from './sections/CoreValuesSection'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+      <HeroSection />
+      <CoreValuesSection />
+      <ProductsSection />
+      <AboutSection />
+      <ReviewsSection />
+      <ContactsSection />
+      <Footer />
+    </div>
   )
 }
+
+export default App
