04d46318ec96c0f0644b5084da41b244f6d00ee2
TrayanStefanov <trayanstefanov95@gmail.com>
2025-09-30 22:38:37 +0300

    Hero section v0.2. Style changes.

--- frontend/src/sections/HeroSection.jsx
+++ frontend/src/sections/HeroSection.jsx
@@ -22,13 +22,13 @@ const HeroSection = () => {
   return (
     <div id="hero" className="relative min-h-[80vh] lg:min-h-screen overflow-hidden flex items-center justify-center bg-base-100">
       {/* Background */}
-            <div className="absolute inset-0 bg-cover bg-center opacity-70" style={{ backgroundImage: "url(/background.png)" }} />
+      <div className="absolute inset-0 bg-cover bg-center opacity-100" style={{ backgroundImage: "url(/bg-2.jpg)" }} />
       <motion.div className="absolute inset-0" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} />
 
       {/* Content */}
-      <div className="relative z-10 text-center text-primary flex flex-col items-center justify-center h-full p-6 md:p-12 lg:p-20 gap-6">
+      <div className="relative z-10 text-center text-secondary flex flex-col items-center justify-between font-emphasis-heading min-h-[80vh] p-6 md:p-12 lg:p-20 gap-6">
         {/* Title build-up */}
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold max-w-4xl flex flex-wrap justify-center leading-tight mt-32 ">
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl max-w-4xl flex flex-wrap justify-center leading-tight">
           {titleLetters.map((letter, index) => (
             <motion.span
               key={index}
@@ -43,65 +43,68 @@ const HeroSection = () => {
         </h1>
 
         {/* Subtitle */}
-        <motion.h2
-          className="text-2xl sm:text-3xl lg:text-4xl max-w-4xl leading-snug"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 1.5, duration: 0.8 }}
-        >
-          {t("home.subtitle")}
-        </motion.h2>
+        <div className="justify-items-center">
 
-        {/* More */}
-        <motion.h3
-          className="text-xl sm:text-2xl lg:text-3xl max-w-4xl font-light"
-          initial={{ opacity: 0, y: 30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 2.2, duration: 0.8 }}
-        >
-          {t("home.more")}
-        </motion.h3>
+          <motion.h2
+            className="text-2xl sm:text-3xl lg:text-4xl max-w-4xl leading-snug mb-12"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.5, duration: 0.8 }}
+          >
+            {t("home.subtitle")}
+          </motion.h2>
 
-        {/* Buttons Desktop */}
-        <div className="hidden lg:flex gap-4 mt-6">
-          <motion.button
-            initial="hiddenLeft"
-            animate="visible"
-            whileHover="hover"
-            variants={buttonVariants}
-            className="btn btn-primary btn-wide rounded-r-lg border border-primary-content text-accent-content text-xl hover:border-accent-content hover:text-primary-content"
-            onClick={() => scrollToSection("#products")}
+          {/* More */}
+          {/* <motion.h3
+            className="text-xl sm:text-2xl lg:text-3xl max-w-4xl font-light"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 2.2, duration: 0.8 }}
           >
-            {t("home.btnProducts")}
-          </motion.button>
-          <motion.button
-            initial="hiddenRight"
-            animate="visible"
-            whileHover="hover"
-            variants={buttonVariants}
-            className="btn btn-primary btn-wide rounded-r-lg border border-primary-content text-accent-content text-xl hover:border-accent-content hover:text-primary-content"
-            onClick={() => scrollToSection("#about")}
-          >
-            {t("home.btnAbout")}
-          </motion.button>
-        </div>
+            {t("home.more")}
+          </motion.h3> */}
 
-        {/* Buttons Mobile */}
-        <div className="flex flex-col lg:hidden w-full mt-6 gap-4">
-          <button
-            onClick={() => scrollToSection("#products")}
-            className="btn btn-primary w-full h-20 sm:h-24 rounded-lg text-2xl sm:text-3xl font-bold shadow-lg"
-            style={{ backgroundImage: "url(/home-projects-btn.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}
-          >
-            {t("home.btnProducts")}
-          </button>
-          <button
-            onClick={() => scrollToSection("#about")}
-            className="btn btn-primary w-full h-20 sm:h-24 rounded-lg text-2xl sm:text-3xl font-bold shadow-lg"
-            style={{ backgroundImage: "url(/home-services-btn.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}
-          >
-            {t("home.btnAbout")}
-          </button>
+          {/* Buttons Desktop */}
+          <div className="hidden lg:flex gap-4 mt-6">
+            <motion.button
+              initial="hiddenLeft"
+              animate="visible"
+              whileHover="hover"
+              variants={buttonVariants}
+              className="btn btn-primary btn-wide rounded-r-lg border border-primary-content text-accent-content text-xl hover:border-accent-content hover:text-primary-content"
+              onClick={() => scrollToSection("#products")}
+            >
+              {t("home.btnProducts")}
+            </motion.button>
+            <motion.button
+              initial="hiddenRight"
+              animate="visible"
+              whileHover="hover"
+              variants={buttonVariants}
+              className="btn btn-primary btn-wide rounded-r-lg border border-primary-content text-accent-content text-xl hover:border-accent-content hover:text-primary-content"
+              onClick={() => scrollToSection("#about")}
+            >
+              {t("home.btnAbout")}
+            </motion.button>
+          </div>
+
+          {/* Buttons Mobile */}
+          <div className="flex flex-col lg:hidden w-full mt-6 gap-4">
+            <button
+              onClick={() => scrollToSection("#products")}
+              className="btn btn-primary w-full h-20 sm:h-24 rounded-lg text-2xl sm:text-3xl font-bold shadow-lg"
+              style={{ backgroundImage: "url(/home-projects-btn.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}
+            >
+              {t("home.btnProducts")}
+            </button>
+            <button
+              onClick={() => scrollToSection("#about")}
+              className="btn btn-primary w-full h-20 sm:h-24 rounded-lg text-2xl sm:text-3xl font-bold shadow-lg"
+              style={{ backgroundImage: "url(/home-services-btn.jpg)", backgroundSize: "cover", backgroundPosition: "center" }}
+            >
+              {t("home.btnAbout")}
+            </button>
+          </div>
         </div>
       </div>
     </div>
