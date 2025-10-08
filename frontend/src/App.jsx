823ac6bb501b294972cba44bef2d389773e0860c
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-08 19:40:22 +0300

    Global gradient added. Removed section backgrounds from all sections.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import HeroSection from './sections/HeroSection'
-import ReviewsSection from './sections/ReviewsSection'
-import ProductsSection from './sections/ProductsSection'
-import AboutSection from './sections/AboutSection'
-import ContactsFAQSection from './sections/ContactsFAQSection'
-import CoreValuesSection from './sections/CoreValuesSection'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HeroSection from "./sections/HeroSection";
+import ReviewsSection from "./sections/ReviewsSection";
+import ProductsSection from "./sections/ProductsSection";
+import AboutSection from "./sections/AboutSection";
+import ContactsFAQSection from "./sections/ContactsFAQSection";
+import CoreValuesSection from "./sections/CoreValuesSection";
 
 function App() {
   return (
-    <div  className=''>
-      <Navbar />
-      <HeroSection />
-      <CoreValuesSection />
-      <ProductsSection />
-      <AboutSection />
-      <ReviewsSection />
-      <ContactsFAQSection />
-      <Footer />
+    <div className="relative min-h-screen overflow-x-hidden">
+      <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
+      <div className="relative z-10">
+        <Navbar />
+        <HeroSection />
+        <CoreValuesSection />
+        <ProductsSection />
+        <AboutSection />
+        <ReviewsSection />
+        <ContactsFAQSection />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
