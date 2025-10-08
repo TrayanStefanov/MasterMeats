d8e031bbbb17961a2e1c67cbec6307b8cb3b880c
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-08 19:57:40 +0300

    Navbar sticky fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,19 @@ import CoreValuesSection from "./sections/CoreValuesSection";
 
 function App() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden">
-      <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
-      <div className="relative z-10">
-        <Navbar />
-        <HeroSection />
-        <CoreValuesSection />
-        <ProductsSection />
-        <AboutSection />
-        <ReviewsSection />
-        <ContactsFAQSection />
-        <Footer />
+    <div>
+      <Navbar />
+      <div className="relative min-h-screen overflow-x-hidden">
+        <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
+        <div className="relative z-10">
+          <HeroSection />
+          <CoreValuesSection />
+          <ProductsSection />
+          <AboutSection />
+          <ReviewsSection />
+          <ContactsFAQSection />
+          <Footer />
+        </div>
       </div>
     </div>
   );
