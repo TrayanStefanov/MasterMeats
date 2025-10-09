2c7edf3ad5891e76e558682283b6aa4bbb2bb129
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-09 19:40:41 +0300

    App ver.0.2 Routes added. Login route added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,23 +9,33 @@ import ProductsSection from "./sections/ProductsSection";
 import AboutSection from "./sections/AboutSection";
 import ContactsFAQSection from "./sections/ContactsFAQSection";
 import CoreValuesSection from "./sections/CoreValuesSection";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="relative min-h-screen overflow-x-hidden">
-        <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
-        <div className="relative z-10">
-          <HeroSection />
-          <CoreValuesSection />
-          <ProductsSection />
-          <AboutSection />
-          <ReviewsSection />
-          <ContactsFAQSection />
-          <Footer />
-        </div>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="relative min-h-screen overflow-x-hidden">
+              <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
+              <div className="relative z-10">
+                <HeroSection />
+                <CoreValuesSection />
+                <ProductsSection />
+                <AboutSection />
+                <ReviewsSection />
+                <ContactsFAQSection />
+              </div>
+            </div>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
