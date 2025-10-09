d3b7ecb7c551627294a7783f2021a10b2ff6b7b9
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-09 19:19:47 +0300

    UserStore v.0.3 Check user authorization.

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -43,6 +43,16 @@ export const useUserStore = create((set, get) => ({
 		} catch (error) {
 			toast.error(error.response?.data?.message || "An error occurred during logout");
 		}
+	},
+
+	checkAuth: async () => {
+		set({ checkingAuth: true });
+		try {
+			const response = await axios.get("/auth/profile");
+			set({ user: response.data, checkingAuth: false });
+		} catch (error) {
+			console.log(error.message);
+			set({ checkingAuth: false, user: null });
+		}
 	}
 }));
-
