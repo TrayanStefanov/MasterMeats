d5072e7b66e5c41c2b1f74d4a437dce00beb264c
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-09 13:36:55 +0300

    About section v0.4. Style changes

--- frontend/src/sections/AboutSection.jsx
+++ frontend/src/sections/AboutSection.jsx
@@ -57,7 +57,7 @@ const HexaTextTile = ({ image, title, paragraphs }) => (
     />
       <h3 className="text-3xl font-bold text-accent">{title}</h3>
       <div className="relative z-10 text-center px-12 py-6 max-w-[70%] text-primary">
-      <div className="text-base leading-relaxed space-y-4 my-8">
+      <div className="text-base leading-relaxed space-y-4 my-8 ">
         {paragraphs.map((p, i) => (
           <p key={i}>{p}</p>
         ))}
