68324b773032510f42eccd6b50ccde1f29cb4ca7
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-10 11:21:17 +0300

    App v0.2.2 Added Signup Modal.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import {React, useState} from "react";
-import { Navigate, Route, Routes } from "react-router";
+import { React, useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -9,27 +8,54 @@ import ProductsSection from "./sections/ProductsSection";
 import AboutSection from "./sections/AboutSection";
 import ContactsFAQSection from "./sections/ContactsFAQSection";
 import CoreValuesSection from "./sections/CoreValuesSection";
+
 import LoginModal from "./components/LoginModal";
+import SignUpModal from "./components/SignUpModal";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
-  const [showLogin, setShowLogin] = useState("");
+  // helper functions
+  const openLoginModal = () => {
+    setShowSignUp(false);
+    setShowLogin(true);
+  };
+
+  const openSignUpModal = () => {
+    setShowLogin(false);
+    setShowSignUp(true);
+  };
+
+  const closeAllModals = () => {
+    setShowLogin(false);
+    setShowSignUp(false);
+  };
 
   return (
     <div>
-      <Navbar onLoginClick={() => setShowLogin(true)} />
-            <div className="relative min-h-screen overflow-x-hidden">
-              <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
-              <div className="relative z-10">
-                <HeroSection />
-                <CoreValuesSection />
-                <ProductsSection />
-                <AboutSection />
-                <ReviewsSection />
-                <ContactsFAQSection />
-              </div>
-                <LoginModal isOpen={showLogin} onClose={() => setShowLogin(false)} />
-            </div>
+      <Navbar onLoginClick={openLoginModal} />
+      <div className="relative min-h-screen overflow-x-hidden">
+        <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30  to-secondary/80" />
+        <div className="relative z-10">
+          <HeroSection />
+          <CoreValuesSection />
+          <ProductsSection />
+          <AboutSection />
+          <ReviewsSection />
+          <ContactsFAQSection />
+        </div>
+        <LoginModal
+          isOpen={showLogin}
+          onClose={closeAllModals}
+          openSignUpModal={openSignUpModal}
+        />
+        <SignUpModal
+          isOpen={showSignUp}
+          onClose={closeAllModals}
+          openLoginModal={openLoginModal}
+        />
+      </div>
       <Footer />
     </div>
   );
