aa49c4728a4323150768e1359fbd42de52ee5585
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-10 15:58:22 +0300

    Zustand CartStore v0.3. Add and remove from cart.

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -16,7 +16,7 @@ export const useCartStore = create((set, get) => ({
             get().calculateTotals();
         } catch (error) {
             set({ cart: [] });
-            toast.error(error.response.data.message || "An error occurred");
+            toast.error(error.response.data.message || "Failed to fetch cart items");
         }
     },
 
@@ -26,5 +26,35 @@ export const useCartStore = create((set, get) => ({
     },
     clearCart: async () => {
         set({ cart: [], coupon: null, total: 0, subtotal: 0 });
-    }
+    },
+    addToCart: async (product) => {
+        try {
+            await axios.post("/cart", { productId: product._id });
+            toast.success("Product added to cart");
+
+            set((prevState) => {
+                const existingItem = prevState.cart.find((item) => item._id === product._id);
+                const newCart = existingItem
+                    ? prevState.cart.map((item) =>
+                        item._id === product._id ? { ...item, quantity: item.quantity + 1 } : item
+                    )
+                    : [...prevState.cart, { ...product, quantity: 1 }];
+                return { cart: newCart };
+            });
+            get().calculateTotals();
+        } catch (error) {
+            toast.error(error.response.data.message || "Failed to add to cart");
+        }
+    },
+    removeFromCart: async (productId) => {
+		try {
+			await axios.delete(`/cart`, { data: { productId } });
+			set((prevState) => ({
+				cart: prevState.cart.filter((item) => item._id !== productId),
+			}));
+			get().calculateTotals();
+		} catch (error) {
+			toast.error(error.response?.data?.message || "Failed to remove item");
+		}
+	}
 }));
