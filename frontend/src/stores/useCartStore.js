edc03d926f04e3cf46a986d1d80400813ca8f488
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-10 15:55:48 +0300

    Zustand CartStore v0.2  Get Cart Info from backend, Clear Cart, Get Items count.

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -8,4 +8,23 @@ export const useCartStore = create((set, get) => ({
     total: 0,
     subtotal: 0,
     isCouponApplied: false,
+
+    getCartItems: async () => {
+        try {
+            const res = await axios.get("/cart");
+            set({ cart: res.data });
+            get().calculateTotals();
+        } catch (error) {
+            set({ cart: [] });
+            toast.error(error.response.data.message || "An error occurred");
+        }
+    },
+
+    getCartCount: () => {
+        const { cart } = get();
+        return cart.reduce((sum, item) => sum + item.quantity, 0);
+    },
+    clearCart: async () => {
+        set({ cart: [], coupon: null, total: 0, subtotal: 0 });
+    }
 }));
