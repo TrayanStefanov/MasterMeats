563dc1b7f148503392a0d70d1f657e73a0a1ec7b
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-10 14:20:28 +0300

    AuthModal v0.5.1. Modal should default to login mode when closed via button or clicking outside.

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
-import { Link } from "react-router-dom";
 import {
   IoLogInOutline,
   IoMailOutline,
@@ -9,12 +8,10 @@ import {
   IoEye,
   IoEyeOff,
   IoClose,
-  IoArrowRedoOutline,
 } from "react-icons/io5";
 import { LuLoader } from "react-icons/lu";
 
 import { useUserStore } from "../stores/useUserStore";
-import { set } from "mongoose";
 
 const AuthModal = ({ isOpen, onClose }) => {
   const [mode, setMode] = useState("login");
@@ -29,7 +26,19 @@ const AuthModal = ({ isOpen, onClose }) => {
   const { login, signup, loading, error, user } = useUserStore();
 
   useEffect(() => {
-    if (user) {
+    if (!isOpen) {
+      setMode("login");
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (user && isOpen) {
       onClose();
     }
   }, [user]);
@@ -58,181 +67,182 @@ const AuthModal = ({ isOpen, onClose }) => {
         isOpen ? "flex" : "hidden"
       } items-center justify-center bg-black/40 backdrop-blur-sm`}
     >
+    <div
+      className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 backdrop-blur-sm"
+      onClick={onClose}
+    >
       <div
-        className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 backdrop-blur-sm"
-        onClick={onClose}
+        className="bg-primary/90 rounded-2xl p-8 w-[90%] max-w-md text-primary-content shadow-lg relative"
+        onClick={(e) => e.stopPropagation()}
       >
-        <div
-          className="bg-primary/90 rounded-2xl p-8 w-[90%] max-w-md text-primary-content shadow-lg relative"
-          onClick={(e) => e.stopPropagation()}
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-accent/70 hover:text-accent transition"
         >
-          <button
-            onClick={onClose}
-            className="absolute top-4 right-4 text-accent/70 hover:text-accent transition"
-          >
-            <IoClose size={24} />
-          </button>
-          <h2 className="text-4xl font-bold text-accent text-center mb-2">
-            {mode === "login" ? "Welcome Back" : "Create an Account"}
-          </h2>
-          <p className="text-center text-primary-content/80 mb-8 text-sm">
-            {mode === "login"
-              ? "Sign in to your account"
-              : "Join us today — it’s quick and easy"}
-          </p>
+          <IoClose size={24} />
+        </button>
 
-          <form onSubmit={handleSubmit} className="space-y-6">
-            {mode === "signup" && (
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-1"
-                >
-                  Full Name
-                </label>
-                <div className="relative">
-                  <IoPersonOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
-                  <input
-                    id="name"
-                    type="text"
-                    required
-                    value={formData.name}
-                    onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
-                    }
-                    placeholder="John Doe"
-                    className="w-full pl-10 pr-3 py-2 rounded-md bg-primary-content/10 text-primary-content 
-                                         border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
-                  />
-                </div>
-              </div>
-            )}
+        <h2 className="text-4xl font-bold text-accent text-center mb-2">
+          {mode === "login" ? "Welcome Back" : "Create an Account"}
+        </h2>
+        <p className="text-center text-primary-content/80 mb-8 text-sm">
+          {mode === "login"
+            ? "Sign in to your account"
+            : "Join us today — it’s quick and easy"}
+        </p>
 
+        <form onSubmit={handleSubmit} className="space-y-6">
+          {mode === "signup" && (
             <div>
-              <label htmlFor="email" className="block text-sm font-medium mb-1">
-                Email Address
+              <label 
+              htmlFor="name" 
+              className="block text-sm font-medium mb-1"
+              >
+                Full Name
               </label>
               <div className="relative">
-                <IoMailOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
+                <IoPersonOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
                 <input
-                  id="email"
-                  type="email"
+                  id="name"
+                  type="text"
                   required
-                  value={formData.email}
+                  value={formData.name}
                   onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
+                    setFormData({ ...formData, name: e.target.value })
                   }
-                  placeholder="you@example.com"
+                  placeholder="John Doe"
                   className="w-full pl-10 pr-3 py-2 rounded-md bg-primary-content/10 text-primary-content 
-                             border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
+                    border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
                 />
               </div>
             </div>
+          )}
 
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium mb-1">
+              Email Address
+            </label>
+            <div className="relative">
+              <IoMailOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
+              <input
+                id="email"
+                type="email"
+                required
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                placeholder="you@example.com"
+                className="w-full pl-10 pr-3 py-2 rounded-md bg-primary-content/10 text-primary-content 
+                  border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label 
+            htmlFor="password" 
+            className="block text-sm font-medium mb-1"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <IoLockClosedOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                placeholder="••••••••"
+                className="w-full pl-10 pr-10 py-2 rounded-md bg-primary-content/10 text-primary-content 
+                  border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-2.5 text-accent/70 hover:text-accent transition"
+              >
+                {showPassword ? <IoEyeOff size={18} /> : <IoEye size={18} />}
+              </button>
+            </div>
+          </div>
+
+          {mode === "signup" && (
             <div>
               <label
-                htmlFor="password"
+                htmlFor="confirmPassword"
                 className="block text-sm font-medium mb-1"
               >
-                Password
+                Confirm Password
               </label>
               <div className="relative">
                 <IoLockClosedOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
                 <input
-                  id="password"
-                  type={showPassword ? "text" : "password"}
+                  id="confirmPassword"
+                  type="password"
                   required
-                  value={formData.password}
+                  value={formData.confirmPassword}
                   onChange={(e) =>
-                    setFormData({ ...formData, password: e.target.value })
+                    setFormData({
+                      ...formData,
+                      confirmPassword: e.target.value,
+                    })
                   }
                   placeholder="••••••••"
-                  className="w-full pl-10 pr-10 py-2 rounded-md bg-primary-content/10 text-primary-content 
-                             border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
+                  className="w-full pl-10 pr-3 py-2 rounded-md bg-primary-content/10 text-primary-content 
+                    border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
                 />
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-3 top-2.5 text-accent/70 hover:text-accent transition"
-                >
-                  {showPassword ? <IoEyeOff size={18} /> : <IoEye size={18} />}
-                </button>
               </div>
             </div>
+          )}
 
-            {mode === "signup" && (
-              <div>
-                <label
-                  htmlFor="confirmPassword"
-                  className="block text-sm font-medium mb-1"
-                >
-                  Confirm Password
-                </label>
-                <div className="relative">
-                  <IoLockClosedOutline className="absolute left-3 top-2.5 w-5 h-5 text-accent/70" />
-                  <input
-                    id="confirmPassword"
-                    type="password"
-                    required
-                    value={formData.confirmPassword}
-                    onChange={(e) =>
-                      setFormData({
-                        ...formData,
-                        confirmPassword: e.target.value,
-                      })
-                    }
-                    placeholder="••••••••"
-                    className="w-full pl-10 pr-3 py-2 rounded-md bg-primary-content/10 text-primary-content 
-                               border border-accent/20 focus:ring-2 focus:ring-accent focus:outline-none transition-all"
-                  />
-                </div>
-              </div>
+          {error && (
+            <p className="text-red-400 text-sm text-center mt-2">{error}</p>
+          )}
+
+          <button
+            type="submit"
+            className="w-full flex justify-center items-center gap-2 py-2 rounded-md bg-accent 
+            text-primary font-semibold hover:bg-accent/80 focus:ring-2 focus:ring-accent/50 
+            transition-all shadow-md shadow-accent/20 disabled:opacity-50"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <LuLoader className="w-5 h-5 animate-spin" />
+                {mode === "login" ? "Logging in..." : "Creating Account..."}
+              </>
+            ) : (
+              <>
+                {mode === "login" ? (
+                  <IoLogInOutline className="w-5 h-5" />
+                ) : (
+                  <IoPersonOutline className="w-5 h-5" />
+                )}
+                {mode === "login" ? "Login" : "Sign Up"}
+              </>
             )}
+          </button>
+        </form>
 
-            {error && (
-              <p className="text-red-400 text-sm text-center mt-2">{error}</p>
-            )}
-
+        <div className="mt-8 text-center text-sm">
+          <p className="text-primary-content/80">
+            {mode === "login"
+              ? "Don’t have an account? "
+              : "Already have an account? "}
             <button
-              type="submit"
-              className="w-full flex justify-center items-center gap-2 py-2 rounded-md bg-accent 
-                                   text-primary font-semibold hover:bg-accent/80 focus:ring-2 focus:ring-accent/50 
-                                   transition-all shadow-md shadow-accent/20 disabled:opacity-50"
-              disabled={loading}
+              type="button"
+              onClick={switchMode}
+              className="text-accent hover:text-accent/80 transition-colors font-medium"
             >
-              {loading ? (
-                <>
-                  <LuLoader className="w-5 h-5 animate-spin" />
-                  {mode === "login" ? "Logging in..." : "Creating Account..."}
-                </>
-              ) : (
-                <>
-                  {mode === "login" ? (
-                    <IoLogInOutline className="w-5 h-5" />
-                  ) : (
-                    <IoPersonOutline className="w-5 h-5" />
-                  )}
-                  {mode === "login" ? "Login" : "Sign Up"}
-                </>
-              )}
+              {mode === "login" ? "Sign up" : "Login"}
             </button>
-          </form>
-
-          <div className="mt-8 text-center text-sm">
-            <p className="text-primary-content/80">
-              {mode === "login"
-                ? "Don’t have an account? "
-                : "Already have an account? "}
-              <button
-                type="button"
-                onClick={switchMode}
-                className="text-accent hover:text-accent/80 transition-colors font-medium"
-              >
-                {mode === "login" ? "Sign up" : "Login"}
-              </button>
-            </p>
-          </div>
+          </p>
         </div>
+      </div>
       </div>
     </div>,
     document.body
