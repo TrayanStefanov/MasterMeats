502582c8d254d00e7287793e100134f6feafb5d5
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-11 12:04:03 +0300

    Server v0.3. Cart routes added.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
 import productRoutes from "./routes/product.routes.js";
 
 import { connectDB } from "./lib/db.js";
@@ -16,6 +17,7 @@ app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/cart", cartRoutes);
 app.use("/api/products", productRoutes);
 
 
