ca75b4eca63d72e806844aea8959877ca021ef7c
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-21 15:25:43 +0300

    PurchaseSuccessModal v0.3.2 Font size. Prettier formating.

--- frontend/src/components/PurchaseCancelModal.jsx
+++ frontend/src/components/PurchaseCancelModal.jsx
@@ -7,15 +7,17 @@ const PurchaseCancelModal = ({ onClose }) => {
 
   return createPortal(
     <div className="fixed inset-0 z-[10000] flex items-center justify-center bg-black/40 backdrop-blur-sm">
-      <div className="bg-secondary border-4 border-accent rounded-lg p-6 w-[90%] max-w-md text-center">
-        <h2 className="text-2xl font-bold mb-4">{t("cart.paymentCancel.title")}</h2>
-        <p className="mb-4">
+      <div className="bg-secondary border-8 border-accent rounded-2xl p-6 w-[90%] max-w-md text-center">
+        <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-bold text-accent mb-4">
+          {t("cart.paymentCancel.title")}
+        </h2>
+        <p className="mb-4 text-xs lg:text-sm 2xl:text-base">
           {t("cart.paymentCancel.subtitle")} <br />
           {t("cart.paymentCancel.subtitle2")}
         </p>
         <button
           onClick={onClose}
-          className="bg-accent text-white px-4 py-2 rounded hover:bg-accent/90 transition"
+          className="bg-accent text-secondary px-4 py-2 rounded-xl hover:bg-accent/90 transition"
         >
           {t("cart.paymentCancel.btn")}
         </button>
