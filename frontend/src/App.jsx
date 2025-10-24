5a3971a5f356af5664007d4c100c1f9138a3b4a5
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-24 22:28:14 +0300

    App v0.5. Admin page added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { React, useState } from "react";
+import { Routes, Route} from "react-router-dom";
+import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HeroSection from "./sections/HeroSection";
-import ReviewsSection from "./sections/ReviewsSection";
 import ProductsSection from "./sections/ProductsSection";
 import AboutSection from "./sections/AboutSection";
+import ReviewsSection from "./sections/ReviewsSection";
 import ContactsFAQSection from "./sections/ContactsFAQSection";
 import CoreValuesSection from "./sections/CoreValuesSection";
 
@@ -14,6 +15,7 @@ import AuthModal from "./modals/AuthModal";
 import CartModal from "./modals/CartModal";
 import BackToTopButton from "./components/BackToTopButton";
 
+import AdminPage from "./admin/AdminPage";
 import { useUserStore } from "./stores/useUserStore";
 
 function App() {
@@ -29,23 +31,39 @@ function App() {
   const closeCartModal = () => setCartModalOpen(false);
 
   return (
-    <div>
-      <Toaster position="top-right"/>
+    <div className="min-h-screen flex flex-col">
+      <Toaster position="top-right" />
       <Navbar onLoginClick={openAuthModal} onCartClick={openCartModal} />
-      <div className="relative min-h-screen overflow-x-hidden">
-        <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30 to-secondary/80" />
-        <div className="relative z-10">
-          <HeroSection />
-          <CoreValuesSection />
-          <ProductsSection />
-          <AboutSection />
-          <ReviewsSection />
-          <ContactsFAQSection />
-        </div>
+
+      <main className="flex-grow relative overflow-x-hidden">
+        <Routes>
+          {/* Public Site */}
+          <Route
+            path="/"
+            element={
+              <div className="relative min-h-screen">
+                <div className="fixed inset-0 bg-gradient-to-b from-secondary/80 via-secondary/30 to-secondary/80" />
+                <div className="relative z-10">
+                  <HeroSection />
+                  <CoreValuesSection />
+                  <ProductsSection />
+                  <AboutSection />
+                  <ReviewsSection />
+                  <ContactsFAQSection />
+                </div>
+              </div>
+            }
+          />
+
+          {/* Admin Dashboard */}
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+
         <AuthModal isOpen={authModalOpen} onClose={closeAuthModal} />
         <CartModal isOpen={cartModalOpen} onClose={closeCartModal} />
         <BackToTopButton />
-      </div>
+      </main>
+
       <Footer />
     </div>
   );
