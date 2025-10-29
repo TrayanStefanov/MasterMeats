9e6eccc98d230d89c15a87f219082931e751db32
TrayanStefanov <trayanstefanov95@gmail.com>
2025-10-29 16:03:26 +0200

    Reservation Tab Subtab OnlineReservations initial commit. v0.1

---
+++ frontend/src/admin/components/OnlineReservations.jsx
@@ -0,0 +1,9 @@
+const OnlineReservations = () => {
+  return (
+    <div className="text-center py-8 text-secondary/60">
+      Online reservations coming soon...
+    </div>
+  );
+};
+
+export default OnlineReservations;
