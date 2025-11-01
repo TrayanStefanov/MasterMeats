ae25de8edf801cc4bb77fe9feab9cfc895f9a2cd
TrayanStefanov <trayanstefanov95@gmail.com>
2025-11-01 22:28:47 +0200

    Client Schema. Initial Commmit. v0.1.1 Phone should be unique and index for faster searches.

--- backend/models/client.model.js
+++ backend/models/client.model.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 const clientSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
-        phone: { type: String, required: true },
+        phone: { type: String, required: true, unique: true },
         email: { type: String },
         user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // If there is a user.
         notes: { type: String },
     },
     { timestamps: true }
 );
+clientSchema.index({ name: 1 });
 
 const Client = mongoose.model("Client", clientSchema);
 export default Client;
