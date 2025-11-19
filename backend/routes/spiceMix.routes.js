0758708868a425274ae84d74fa2cf282c1133b43
TrayanStefanov <trayanstefanov95@gmail.com>
2025-11-19 22:54:35 +0200

    SpiceMix routes v0.1.1. getAllTags and updateStock routes.

--- backend/routes/spiceMix.routes.js
+++ backend/routes/spiceMix.routes.js
@@ -4,15 +4,19 @@ import {
   getSpiceMixes,
   getSpiceMix,
   updateSpiceMix,
+  addStockToMix,
   deleteSpiceMix,
+  getAllTags
 } from "../controllers/spiceMix.controller.js";
 
 const router = express.Router();
 
 router.post("/", createSpiceMix);
 router.get("/", getSpiceMixes);
+router.get("/tags", getAllTags);
 router.get("/:id", getSpiceMix);
 router.put("/:id", updateSpiceMix);
+router.put("/:id/addStock", addStockToMix);
 router.delete("/:id", deleteSpiceMix);
 
 export default router;
