b5a284b6636dd78da092ce829d3e0852036a9deb
TrayanStefanov <trayanstefanov95@gmail.com>
2025-11-21 21:43:42 +0200

    Spice routes v0.1.1 Admin only routes.

--- backend/routes/spice.routes.js
+++ backend/routes/spice.routes.js
@@ -6,13 +6,14 @@ import {
   updateSpice,
   deleteSpice,
 } from "../controllers/spice.controller.js";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", createSpice);
-router.get("/", getSpices);
-router.get("/:id", getSpice);
-router.put("/:id", updateSpice);
-router.delete("/:id", deleteSpice);
+router.post("/", protectRoute, adminRoute, createSpice);
+router.get("/", protectRoute, adminRoute, getSpices);
+router.get("/:id", protectRoute, adminRoute, getSpice);
+router.put("/:id", protectRoute, adminRoute, updateSpice);
+router.delete("/:id", protectRoute, adminRoute, deleteSpice);
 
 export default router;
