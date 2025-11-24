28eded0f5f01bb4cb20efe17a15313fbb35b4b42
TrayanStefanov <trayanstefanov95@gmail.com>
2025-11-24 18:20:02 +0200

    Batch routes v0.2. Phase Specific routes added.

--- backend/routes/batch.routes.js
+++ backend/routes/batch.routes.js
@@ -7,22 +7,27 @@ import {
   addSeasoningEntry,
   addVacuumEntry,
   finishBatch,
-  deleteBatch
+  deleteBatch,
+  updateSourcingPhase,
+  updatePreppingPhase,
+  updateCuringPhase
 } from "../controllers/batch.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 /* ----------------------- BATCH CRUD ----------------------- */
-router.post("/",protectRoute, adminRoute, createBatch);
-router.get("/",protectRoute, adminRoute, getBatches);
-router.get("/:id",protectRoute, adminRoute, getBatch);
-router.put("/:id",protectRoute, adminRoute, updateBatch);
-router.delete("/:id",protectRoute, adminRoute, deleteBatch);
+router.post("/", protectRoute, adminRoute, createBatch);
+router.get("/", protectRoute, adminRoute, getBatches);
+router.get("/:id", protectRoute, adminRoute, getBatch);
+router.delete("/:id", protectRoute, adminRoute, deleteBatch);
 
-/* --------------------- STAGE-SPECIFIC ---------------------- */
-router.post("/:id/seasoning",protectRoute, adminRoute, addSeasoningEntry);
-router.post("/:id/vacuum",protectRoute, adminRoute, addVacuumEntry);
-router.post("/:id/finish",protectRoute, adminRoute, finishBatch);
+/* --------------------- PHASE-SPECIFIC --------------------- */
+router.put("/:id/sourcing", protectRoute, adminRoute, updateSourcingPhase);
+router.put("/:id/prepping", protectRoute, adminRoute, updatePreppingPhase);
+router.put("/:id/curing", protectRoute, adminRoute, updateCuringPhase);
+router.post("/:id/seasoning", protectRoute, adminRoute, addSeasoningEntry);
+router.post("/:id/vacuum", protectRoute, adminRoute, addVacuumEntry);
+router.post("/:id/finish", protectRoute, adminRoute, finishBatch);
 
 export default router;
